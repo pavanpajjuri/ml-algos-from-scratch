7a6ec0a53deea1883031478a7f29f8ccb5e21d5a
pavanpajjuri <pavan.pajjuri99@gmail.com>
2025-04-10 21:13:31 -0400

    Prediction Code Added and Accuracy Metrics added

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -41,6 +41,9 @@ class Node():
         self.right = right
         self.gain = gain
         self.value = value
+    
+    def __repr__(self):
+        return f"Node(feature = {self.feature}, threshold = {self.threshold}, value = {self.value})"
 
 class DecisionTree():
     def __init__(self,min_samples = 2, max_depth = 2):
@@ -97,16 +100,111 @@ class DecisionTree():
             if best_split['gain']:
                 left_node = self.build_tree(best_split['left_dataset'], depth + 1)
                 right_node = self.build_tree(best_split['right_dataset'], depth + 1)
-                return Node(best_split['feature'], best_split['threshold'], left_node, right_node, best_split['gain'])
+                return Node(
+                            feature=best_split['feature'],
+                            threshold=best_split['threshold'],
+                            left=left_node,
+                            right=right_node,
+                            gain=best_split['gain'],
+                            value=None
+                            )
         y = list(y)
         value = max(y, key = y.count)
-        return Node(value)
+        return Node(
+                feature=None,
+                threshold=None,
+                left=None,
+                right=None,
+                gain=None,
+                value=value
+            )
         
         
     def fit(self, X, y):
         dataset = np.concatenate((X,y.reshape(-1,1)), axis = 1)
         self.root = self.build_tree(dataset)
+        
+    def predict(self, X):
+        return np.array([self.predict_single(x, self.root) for x in X])
+    
+    def predict_single(self, x, Node):
+        if Node.value is not None:
+            return Node.value
+        else:
+            feature = x[int(Node.feature)]
+            if feature <= Node.threshold:
+                return self.predict_single(x, Node.left)
+            if feature > Node.threshold:
+                return self.predict_single(x, Node.right)
             
+            
+
+class ClassificationMetrics:
+    @staticmethod 
+    def accuracy(y_true, y_pred):
+        return np.sum(y_true == y_pred)/len(y_true)
+    
+    @staticmethod  
+    def precision(y_true, y_pred):
+        true_positives = np.sum((y_true == 1) & (y_pred == 1))
+        false_positives = np.sum((y_true == 0) & (y_pred == 1))
+        if (true_positives + false_positives) == 0:
+            return 0
+        return np.divide(true_positives, (true_positives + false_positives))
+    
+    @staticmethod 
+    def recall(y_true, y_pred):
+        true_positives = np.sum((y_true == 1) & (y_pred == 1))
+        false_negatives = np.sum((y_true == 1) & (y_pred == 0))
+        if (true_positives + false_negatives) == 0: 
+            return 0
+        return np.divide(true_positives, (true_positives + false_negatives))
+    
+    @staticmethod 
+    def f1_score(y_true, y_pred):
+        precision = ClassificationMetrics.precision(y_true, y_pred)
+        recall = ClassificationMetrics.recall(y_true, y_pred)
+        if (precision + recall) == 0:
+            return 0
+        return np.divide(2*precision*recall, (precision + recall))
+    
+    @staticmethod 
+    def perf_metrics(y_true, y_pred_prob, threshold = 0.5):
+        tp = fp = tn = fn = 0
+        y_pred = np.where(y_pred_prob > threshold, 1, 0)
+        tp = np.sum((y_true == 1) & (y_pred == 1))
+        fp = np.sum((y_true == 0) & (y_pred == 1))
+        tn = np.sum((y_true == 0) & (y_pred == 0))
+        fn = np.sum((y_true == 1) & (y_pred == 0))
+        
+        tpr = tp/(tp + fn) if (tp + fn) > 0 else 0 # Sensitivity
+        fpr = fp/(fp + tn) if (fp + tn) > 0 else 0 # 1- Specificity
+        return fpr, tpr
+    
+    @staticmethod 
+    def roc_auc_score(y_true, y_pred_prob, plot = False):
+       thresholds = np.linspace(0, 1, 100)
+       fpr_list, tpr_list = [], []
+       
+       for threshold in thresholds:
+           fpr, tpr = ClassificationMetrics.perf_metrics(y_true, y_pred_prob, threshold = threshold)
+           fpr_list.append(fpr)
+           tpr_list.append(tpr)
+           
+       sorted_pairs = sorted(zip(fpr_list, tpr_list)) # Sort by Fprlist
+       fpr_sorted, tpr_sorted = zip(*sorted_pairs)
+       auc = np.trapz(tpr_sorted, fpr_sorted)
+       
+       if plot:
+           plt.plot(fpr_sorted, tpr_sorted, 'r-', lw=2)
+           plt.plot([0, 1], [0, 1], 'k--', lw=1)
+           plt.xlabel("False Positive Rate")
+           plt.ylabel("True Positive Rate")
+           plt.title(f"ROC Curve (AUC = {auc:.3f})")
+           plt.grid(True)
+           plt.show()
+           
+       return auc
 
 X_train, y_train = train_data.iloc[:,:-1].values, train_data.iloc[:,-1].values
 X_test, y_test = test_data.iloc[:,:-1].values, test_data.iloc[:,-1].values
@@ -114,8 +212,20 @@ X_test, y_test = test_data.iloc[:,:-1].values, test_data.iloc[:,-1].values
 
 dt = DecisionTree()
 dt.fit(X_train,y_train)
+y_pred = dt.predict(X_test)
     
+test_accuracy = ClassificationMetrics.accuracy(y_test, y_pred)
+test_precision = ClassificationMetrics.precision(y_test, y_pred)
+test_recall = ClassificationMetrics.recall(y_test, y_pred)
+test_f1_score = ClassificationMetrics.f1_score(y_test, y_pred)
+#test_auc_score = ClassificationMetrics.roc_auc_score(y_test, y_pred_prob, plot = True)
 
+
+print(f"Final testing Accuracy Score: {test_accuracy:.4f}")
+print(f"Final testing Precision Score: {test_precision:.4f}")
+print(f"Final testing Recall Score: {test_recall:.4f}")
+print(f"Final testing f1_score Score: {test_f1_score:.4f}")
+#print(f"Final Training roc_auc Score: {test_auc_score:.4f}")
 
 
 
