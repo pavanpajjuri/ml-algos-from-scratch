b9f7b80990993aaf4552c58c2947a091dd3992a0
pavanpajjuri <pavan.pajjuri99@gmail.com>
2025-04-20 18:51:48 -0400

    Adding Changes for executing script and calling for other scripts

--- RandomForest.py
+++ RandomForest.py
@@ -12,12 +12,6 @@ from sklearn.datasets import load_breast_cancer
 
 from DecisionTrees import DecisionTree, ClassificationMetrics
 
-seed = 69
-np.random.seed(seed = seed)
-
-data = load_breast_cancer()
-X = pd.DataFrame(data = data.data, columns = data.feature_names)
-y = pd.Series(data = data.target, name = 'target')
 
 
 
@@ -28,11 +22,6 @@ def remove_lowly_correlated_features(X,y,threshold = 0.05):
     X = X.drop(columns = low_info_cols)
     return X,y
 
-X,y = remove_lowly_correlated_features(X, y)
-
-data = pd.concat([X,y], axis = 1)
-train_data = data[:int(0.8*len(data))]
-test_data = data[int(0.8*len(data)):]
 
 class RandomForest:
     def __init__(self, n_trees = 7, max_depth = 4, min_samples = 2):
@@ -65,54 +54,67 @@ class RandomForest:
         
         return majority_preds, pred_prob
     
-
-X_train,y_train = train_data.iloc[:,:-1].values,train_data.iloc[:,-1].values
-X_test,y_test = test_data.iloc[:,:-1].values,test_data.iloc[:,-1].values
-
-
-
-rf = RandomForest(n_trees = 7, max_depth = 4, min_samples = 2)
-rf.fit(X_train,y_train)
-
-y_pred, y_pred_prob = rf.predict(X_test)
-
-
-
-test_accuracy = ClassificationMetrics.accuracy(y_test, y_pred)
-test_precision = ClassificationMetrics.precision(y_test, y_pred)
-test_recall = ClassificationMetrics.recall(y_test, y_pred)
-test_f1_score = ClassificationMetrics.f1_score(y_test, y_pred)
-test_auc_score = ClassificationMetrics.roc_auc_rank_based(y_test, y_pred_prob)
-
-
-print(f"Final testing Accuracy Score: {test_accuracy:.4f}")
-print(f"Final testing Precision Score: {test_precision:.4f}")
-print(f"Final testing Recall Score: {test_recall:.4f}")
-print(f"Final testing f1_score Score: {test_f1_score:.4f}")
-print(f"Final Training roc_auc Score: {test_auc_score:.4f}")
-
-
-
-
-"""from sklearn.ensemble import RandomForestClassifier
-
-rf = RandomForestClassifier(n_estimators = 7, max_depth = 4,random_state = seed,min_samples_split=2)
-rf.fit(X_train, y_train)
-
-y_pred = rf.predict(X_test)
-y_pred_prob = rf.predict_proba(X_test)[:,1]
-
-etst_accuracy = ClassificationMetrics.accuracy(y_test, y_pred)
-test_precision = ClassificationMetrics.precision(y_test, y_pred)
-test_recall = ClassificationMetrics.recall(y_test, y_pred)
-test_f1_score = ClassificationMetrics.f1_score(y_test, y_pred)
-test_auc_score = ClassificationMetrics.roc_auc_rank_based(y_test, y_pred_prob)
-
-print()
-print(f"Final testing Accuracy Score: {test_accuracy:.4f}")
-print(f"Final testing Precision Score: {test_precision:.4f}")
-print(f"Final testing Recall Score: {test_recall:.4f}")
-print(f"Final testing f1_score Score: {test_f1_score:.4f}")
-print(f"Final Training roc_auc Score: {test_auc_score:.4f}")"""
+if __name__ == "__main__":
+    seed = 69
+    np.random.seed(seed = seed)
+    
+    data = load_breast_cancer()
+    X = pd.DataFrame(data = data.data, columns = data.feature_names)
+    y = pd.Series(data = data.target, name = 'target')
+    
+    X,y = remove_lowly_correlated_features(X, y)
+    
+    data = pd.concat([X,y], axis = 1)
+    train_data = data[:int(0.8*len(data))]
+    test_data = data[int(0.8*len(data)):]
+    
+    X_train,y_train = train_data.iloc[:,:-1].values,train_data.iloc[:,-1].values
+    X_test,y_test = test_data.iloc[:,:-1].values,test_data.iloc[:,-1].values
+    
+    
+    
+    rf = RandomForest(n_trees = 7, max_depth = 4, min_samples = 2)
+    rf.fit(X_train,y_train)
+    
+    y_pred, y_pred_prob = rf.predict(X_test)
+    
+    
+    
+    test_accuracy = ClassificationMetrics.accuracy(y_test, y_pred)
+    test_precision = ClassificationMetrics.precision(y_test, y_pred)
+    test_recall = ClassificationMetrics.recall(y_test, y_pred)
+    test_f1_score = ClassificationMetrics.f1_score(y_test, y_pred)
+    test_auc_score = ClassificationMetrics.roc_auc_rank_based(y_test, y_pred_prob)
+    
+    
+    print(f"Final testing Accuracy Score: {test_accuracy:.4f}")
+    print(f"Final testing Precision Score: {test_precision:.4f}")
+    print(f"Final testing Recall Score: {test_recall:.4f}")
+    print(f"Final testing f1_score Score: {test_f1_score:.4f}")
+    print(f"Final testing roc_auc Score: {test_auc_score:.4f}")
+    
+    
+    
+    
+    """from sklearn.ensemble import RandomForestClassifier
+    
+    rf = RandomForestClassifier(n_estimators = 7, max_depth = 4,random_state = seed,min_samples_split=2)
+    rf.fit(X_train, y_train)
+    
+    y_pred = rf.predict(X_test)
+    y_pred_prob = rf.predict_proba(X_test)[:,1]
+    
+    etst_accuracy = ClassificationMetrics.accuracy(y_test, y_pred)
+    test_precision = ClassificationMetrics.precision(y_test, y_pred)
+    test_recall = ClassificationMetrics.recall(y_test, y_pred)
+    test_f1_score = ClassificationMetrics.f1_score(y_test, y_pred)
+    test_auc_score = ClassificationMetrics.roc_auc_rank_based(y_test, y_pred_prob)
+    
+    print()
+    print(f"Final testing Accuracy Score: {test_accuracy:.4f}")
+    print(f"Final testing Precision Score: {test_precision:.4f}")
+    print(f"Final testing Recall Score: {test_recall:.4f}")
+    print(f"Final testing f1_score Score: {test_f1_score:.4f}")
+    print(f"Final Training roc_auc Score: {test_auc_score:.4f}")"""
         
         
