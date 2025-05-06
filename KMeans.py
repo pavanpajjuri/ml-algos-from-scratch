aa11edd983736e3749d0cd73e3ba67c6cd2e1919
pavanpajjuri <pavan.pajjuri99@gmail.com>
2025-05-05 23:04:37 -0400

    KMeans Code Added

--- KMeans.py
+++ KMeans.py
@@ -6,3 +6,63 @@ Created on Sun May  4 23:55:05 2025
 @author: pavanpaj
 """
 
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+
+class KMeans:
+    def __init__(self, K):
+        self.K = K
+    
+    def initialize_centroids(self, X):
+        centroid_idx = np.random.permutation(X.shape[0])[:K]
+        self.centroids = X[centroid_idx]
+    
+    def assign_points_to_centroids(self, X):
+        X_expanded =  X[:,np.newaxis,:]
+        centroids_expanded = self.centroids[np.newaxis,:,:]
+        diff = X_expanded - centroids_expanded
+        dist = np.sqrt(np.sum(diff**2, axis = 2))
+        points = np.argmin(dist, axis = 1)
+        return points
+    
+    def compute_mean(self, X, points):
+        centroids = np.zeros((self.K, X.shape[1]))
+        for i in range(self.K):
+            if np.any(points == i):
+                centroids[i] = X[points == i].mean(axis=0)
+            else:
+                centroids[i] = X[np.random.randint(0, X.shape[0])]
+        return centroids
+        
+    def fit(self, X, iterations = 10):
+        self.initialize_centroids(X)
+        for i in range(iterations):
+            points = self.assign_points_to_centroids(X)
+            new_centroids = self.compute_mean(X, points)
+            if np.allclose(self.centroids, new_centroids):
+                break
+            self.centroids = new_centroids
+        return self.centroids, points
+    
+    def predict(self, X_new):
+        return self.assign_points_to_centroids(X_new)
+
+if __name__ == "__main__":
+    seed = 69
+    np.random.seed(seed = seed)
+    
+    data = load_iris()
+    X = pd.DataFrame(data = data.data, columns = data.feature_names)
+    y = pd.Series(data = data.target, name = 'target')
+    
+    X = np.array(X)
+    K = 3
+        
+    kmeans = KMeans(K = K)
+    centroids, points = kmeans.fit(X, 100)
+        
+    
+    
+    
